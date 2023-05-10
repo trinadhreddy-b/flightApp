6875c4a4e2d38b2e1279bdb667c29701c803c326
trinadh reddy bheemireddy <trinadhreddy.b@gmail.com>
2023-05-10 17:40:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
 
-import Booking from "./components/Flights";
 import AdminDashboard from "./components/AdminDashboard";
 
 import { ThemeProvider } from "@emotion/react";
@@ -16,19 +15,18 @@ import { FlightsProvider } from "./context/FlightsProvider";
 
 function App() {
   return (
-    
     <ThemeProvider theme={theme}>
-      <FlightsProvider >
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/flights" element={<Flights />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/admin" element={<AdminDashboard />} />
-          <Route exact path="/success" element={<BookingConfirmation />} />
-        </Routes>
-      </BrowserRouter>
+      <FlightsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/flights" element={<Flights />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/admin" element={<AdminDashboard />} />
+            <Route exact path="/success" element={<BookingConfirmation />} />
+          </Routes>
+        </BrowserRouter>
       </FlightsProvider>
     </ThemeProvider>
   );
